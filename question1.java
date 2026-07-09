9e60aaf41c347fa56e562e8c776b2afa05a6452e
Sai Rachakula <z4professionalworkaccnt@gmail.com>
2026-07-09 15:10:31 +0530

    Correct Java syntax and import statements
    Fix syntax errors and improve import statements.

--- question1.java
+++ question1.java
@@ -1,1 +1,18 @@
+//Question 1
+import java.util.Scanner;
+import java.util.io*;
 
+public agilecode{
+    public static void main (string args[]){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello Techies, Welcome to WEB\n");
+        System.out.println("Please enter your username and password to login\n");
+            System.out.println("Username: ");
+            string username = scanner.nextLine();
+            System.out.println("\nPassword: ")
+            string password = scanner.nextLine();
+            
+
+
+    }
+}

4fc643db69ef2935640e78d7f6d4ac1809ae7d3f
Zaheer Rachakula <z4professionalworkaccnt@gmail.com>
2026-07-29 14:20:46 +0530

    Create helloworld.java with main method
    Fix syntax errors and add missing imports.

---
+++ helloworld.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.util.Scanner;
+
+public helloworld{
+
+public static void main (String args[]){
+System.out.println("Hello world! welcome to java\n print by jenkins through run...:);
+
+}
+}

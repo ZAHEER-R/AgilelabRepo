093fbb60cbeb2505710d48a93c1f0d4a2bf6aca4
Zaheer Rachakula <z4professionalworkaccnt@gmail.com>
2026-07-29 14:28:57 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.util.Scanner;
 
-public helloworld{
+public class helloworld{
 
 public static void main (String args[]){
 System.out.println("Hello world! welcome to java\n print by jenkins through run...:);
